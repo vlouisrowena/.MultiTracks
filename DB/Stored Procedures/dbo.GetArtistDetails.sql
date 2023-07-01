e8ba7c906c5d4942fe2af24a4e00176c823224c6
v.louisrowena <louisrowena.vuppula@mavs.uta.edu>
2023-07-01 13:49:42 -0500

    Create artistDetails page and add GetArtistDetails.sql for data retrieval

---
+++ DB/Stored Procedures/dbo.GetArtistDetails.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE dbo.GetArtistDetails
+	@artistId int = -1
+	
+AS
+BEGIN
+
+	SELECT Album.title AS albumTitle, Album.imageURL AS aLbumImage, Album.year AS albumYear,
+       Song.title AS songTitle, Song.bpm AS songBpm, Song.timeSignature AS songTimeSignature,
+	   Artist.title AS artistTitle, Artist.heroURL AS artistHeroImg, Artist.imageURL AS artistImg, Artist.biography AS artistBiography
+	FROM Artist
+	LEFT JOIN Album ON Album.artistID = ARTIST.artistID
+	LEFT JOIN Song ON Song.albumID = Album.albumID
+	WHERE Artist.artistID = @artistId
+	ORDER BY Song.title, Album.title
+
+	
+END
---
+++ Web/multitracks.com/multitracks.com/artistDetails.aspx.cs
@@ -0,0 +1,37 @@
+﻿using DataAccess;
+using System;
+using System.Activities.Expressions;
+using System.Drawing;
+using System.Web.UI.WebControls;
+
+public partial class ArtistDetails : MultitracksPage
+{
+    protected string Biography { get; set; }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var sql = new SQL();
+        sql.Parameters.Add("@artistId", 5);
+        var data = sql.ExecuteStoredProcedureDT("GetArtistDetails");
+        
+        if (data.Rows.Count > 0)
+        {
+            
+            artistBanner.DataSource = data.Rows[0].Table;          
+            artistBanner.DataBind();
+
+            Biography = data.Rows[0]["artistBiography"].ToString();       
+
+            songList.DataSource = data; 
+            songList.DataBind();
+
+            albumList.DataSource = data;
+            albumList.DataBind();
+
+
+        }
+
+
+
+
+    }
+}
