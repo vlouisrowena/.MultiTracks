f6799a1702c29b9cc8f083a61400c50d66a11ec0
v.louisrowena <louisrowena.vuppula@mavs.uta.edu>
2023-07-05 12:59:00 -0500

    Create api project and add required packages.

---
+++ MultiTracksAPI/Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+builder.Services.AddControllers();
+builder.Services.AddDbContext<MultiTracksDBContext>(options =>
+            options.UseSqlServer(configuration.GetValue<string>("MultiTracksDatabase:ConnectionStrings")));
+
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<DbContext, MultiTracksDBContext>();
+builder.Services.AddScoped<IMultiTracksDBContextProcedures, MultiTracksDBContextProcedures>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
