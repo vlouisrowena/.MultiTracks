74b8b11c7b7fc8bb2f5adbf8dd4bb59256ccfef3
v.louisrowena <louisrowena.vuppula@mavs.uta.edu>
2023-07-06 02:46:10 -0500

    Add SongController.cs and add paging functionality

---
+++ MultiTracksAPI/Controllers/SongController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MultiTracksAPI.Models;
+
+
+namespace MultiTracksAPI.Controllers
+{
+    [Route("song")]
+    [ApiController]
+    public class SongController : Controller
+    {
+        private readonly MultiTracksDBContext _dbContext;
+
+        public SongController(MultiTracksDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        [HttpGet("list")]
+
+        public async Task<IActionResult> SongList([FromQuery] PageParams songParams)
+        {
+            var songs = await (from song in _dbContext.Song
+
+                             select song.Title
+
+                             ).Skip((songParams.pageNumber - 1) * songParams.PageSize)
+                              .Take(songParams.PageSize)
+                              .ToListAsync();
+            return Ok(songs);
+        }
+    }
+
+}
---
+++ MultiTracksAPI/Models/PageParams.cs
@@ -0,0 +1,21 @@
+﻿namespace MultiTracksAPI.Models
+{
+    public class PageParams
+    {
+
+        const int maxPageSize = 30; 
+        public int pageNumber { get; set; } = 1;
+        private int _pageSize = 10;  
+        public int PageSize
+        {
+            get
+            {
+                return _pageSize;
+            }
+            set
+            {
+                _pageSize = (value > maxPageSize) ? maxPageSize : value;
+            }
+        }
+    }
+}
